84d12ae28d6af0ef67e5d396d1d5fb130b618c3f
John <john@iamjohnhenry.com>
2023-12-19 19:09:28 -0800

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,63 @@
+declare module "astro-plain";
+
+export type Meta = {
+  title?: string;
+  description?: string;
+  keywords?: string;
+  author?: string;
+  image?: string;
+  [key: string]: string | undefined;
+};
+
+export type HeadProps = {
+  charSet?: string;
+  title?: string;
+  titleJoiner?: string;
+  author?: string;
+  canonical?: string;
+  serviceWorkerScript?: string;
+  manifest?: string;
+  favicon?: string;
+  faviconType?: string;
+  tmid?: string;
+  appleTouchIcons?: string[];
+  webComponents?: string[];
+  externalStyles?: string[];
+  externalScripts?: string[];
+  criticalStyles?: string;
+  criticalScripts?: string;
+  image?: string;
+  meta?: Meta;
+  lang?: string;
+  twitterMeta?: boolean;
+  ogMeta?: boolean;
+};
+
+export type HeadComponent = (props: HeadProps) => string;
+
+export type Props = {
+  title?: string;
+  titleJoiner?: string;
+  charSet?: string;
+  description?: string;
+  keywords?: string;
+  author?: string;
+  canonical?: string;
+  class?: string;
+  serviceWorkerScript?: string;
+  manifest?: string;
+  favicon?: string;
+  faviconType?: string;
+  tmid?: string;
+  appleTouchIcons?: string[];
+  webComponents?: string[];
+  externalStyles?: string[];
+  externalScripts?: string[];
+  criticalStyles?: string;
+  criticalScripts?: string;
+  image?: string;
+  meta?: Meta;
+  lang?: string;
+  twitterMeta?: boolean;
+  ogMeta?: boolean;
+};
